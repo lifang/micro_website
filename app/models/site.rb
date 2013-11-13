ec30750802c02ad2225ce5ebcfeb78ae6bf8954d
wangping <525399584@qq.com>
2013-11-13 09:44:34 +0800

    site

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Site < ActiveRecord::Base
   has_many :resources
   has_many :pages
@@ -9,14 +8,3 @@ class Site < ActiveRecord::Base
    uniqueness: { case_sensitive: false },
    format:{with:/[a-zA-Z]/i}
 end
-=======
-class Site < ActiveRecord::Base
-  has_many :pages
-  attr_accessible :name, :root_path, :notes, :user_id
-  validates :name ,presence:true,uniqueness: { case_sensitive: false }
-  validates :root_path ,
-   presence:true,
-   uniqueness: { case_sensitive: false },
-   format:{with:/[a-zA-Z]/i}
-end
->>>>>>> 48503c02396c467b27a369a4a75b461ac4243441
