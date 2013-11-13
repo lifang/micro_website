b40cffab41f360cf3db9b320528a4a280a5a1998
amanda <mengfanling825@163.com>
2013-11-14 00:21:38 +0800

    amanda 11-14-00

--- app/assets/javascripts/page.js
+++ app/assets/javascripts/page.js
@@ -24,59 +24,55 @@ function hide_tab(obj){
     $(".second_bg").hide();
 }
 
-var inputEle = "<div class='insertBox inputBox'><span class='delete'></span><div class='inputArea' ondblclick='showInput(this)'>双击输入问题</div><input class='txtArea textQuestion' type='text' onblur='hideInput(this)' /><input type='text' class='newNameClass' /></div>";
+var inputEle = "<div class='insertBox inputBox'><span class='delete' onclick='deleOption(this)'></span><div class='inputArea' ondblclick='showInput(this)'>双击输入问题</div><input class='txtArea textQuestion' type='text' onblur='hideInput(this)' /><input type='text' class='newNameClass' /></div>";
 
-var radioEle = "<div class='insertBox radioBox'><span class='delete'></span><div class='inputArea' ondblclick='showInput(this)'>双击输入问题</div><input class='txtArea textQuestion' type='text' onblur='hideInput(this)' /><div><input type='radio' class='newNameClass' /><div class='inputArea' ondblclick='showInput(this)'>双击输入选项</div><input class='txtArea' type='text' onblur='hideInput(this)'/></div><div><input type='radio' class='newNameClass' /><div class='inputArea' ondblclick='showInput(this)'>双击输入选项</div><input class='txtArea' type='text' onblur='hideInput(this)'/></div></div>";
+var radioEle = "<div class='insertBox radioBox'><span class='delete' onclick='deleOption(this)'></span><span class='add_option' onclick='addOption(this, 2)'>添加选项</span><div class='inputArea' ondblclick='showInput(this)'>双击输入问题</div><input class='txtArea textQuestion' type='text' onblur='hideInput(this)' /><div><input type='radio' class='newNameClass' /><div class='inputArea' ondblclick='showInput(this)'>双击输入选项</div><input class='txtArea' type='text' onblur='hideInput(this)'/><span class='deleteOption' onclick='deleOption(this)'>去除选项</span></div><div><input type='radio' class='newNameClass' /><div class='inputArea' ondblclick='showInput(this)'>双击输入选项</div><input class='txtArea' type='text' onblur='hideInput(this)'/><span class='deleteOption' onclick='deleOption(this)'>去除选项</span></div></div>";
 
-var checkboxEle = "<div class='insertBox checkboxBox'><span class='delete'></span><div class='inputArea' ondblclick='showInput(this)'>双击输入问题</div><input class='txtArea textQuestion' type='text' onblur='hideInput(this)'/><div><input type='checkbox' class='newNameClass'/><div class='inputArea' ondblclick='showInput(this)'>双击输入选项</div><input class='txtArea' type='text' onblur='hideInput(this)'/></div><div><input type='checkbox' class='newNameClass'/><div class='inputArea' ondblclick='showInput(this)'>双击输入选项</div><input class='txtArea' type='text' onblur='hideInput(this)'/></div></div>";
+var checkboxEle = "<div class='insertBox checkboxBox'><span class='delete' onclick='deleOption(this)'></span><span class='add_option' onclick='addOption(this, 3)'>添加选项</span><div class='inputArea' ondblclick='showInput(this)'>双击输入问题</div><input class='txtArea textQuestion' type='text' onblur='hideInput(this)'/><div><input type='checkbox' class='newNameClass'/><div class='inputArea' ondblclick='showInput(this)'>双击输入选项</div><input class='txtArea' type='text' onblur='hideInput(this)'/><span class='deleteOption' onclick='deleOption(this)'>去除选项</span></div><div><input type='checkbox' class='newNameClass'/><div class='inputArea' ondblclick='showInput(this)'>双击输入选项</div><input class='txtArea' type='text' onblur='hideInput(this)'/><span class='deleteOption' onclick='deleOption(this)'>去除选项</span></div></div>";
 
-var radioOption = "<div><input type='radio' /><div class='inputArea' ondblclick='showInput(this)'>双击输入选项</div><input class='txtArea' type='text' onblur='hideInput(this)'/></div>";
+var radioOption = "<div><input type='radio' class='newNameClass' /><div class='inputArea' ondblclick='showInput(this)'>双击输入选项</div><input class='txtArea' type='text' onblur='hideInput(this)'/><span class='deleteOption' onclick='deleOption(this)'>去除选项</span></div>";
 
-var checkboxOption = "<div><input type='checkbox' /><div class='inputArea' ondblclick='showInput(this)'>双击输入选项</div><input class='txtArea' type='text' onblur='hideInput(this)'/></div>";
+var checkboxOption = "<div><input type='checkbox' class='newNameClass'/><div class='inputArea' ondblclick='showInput(this)'>双击输入选项</div><input class='txtArea' type='text' onblur='hideInput(this)'/><span class='deleteOption' onclick='deleOption(this)'>去除选项</span></div>";
 
 function addQuestion(type){
     var newEle;
     switch(type)
     {
-        case 1:
-
+        case 1: //input
             $(".insertDiv").append(inputEle);
             var inputCount = $(".insertDiv").find(".inputBox").length;
             newEle = $(".insertDiv .insertBox ").last();
-
             newEle.find(".textQuestion").attr("name", "form[input_" + inputCount + "_value]");
             newEle.find(".newNameClass").attr("name", "form[input_" + inputCount + "]");
             break;
-        case 2:
-
+        case 2: //radio
             $(".insertDiv").append(radioEle);
             var radioCount = $(".insertDiv").find(".radioBox").length;
-
             newEle = $(".insertDiv .insertBox ").last();
             newEle.find(".textQuestion").attr("name", "form[radio_" + radioCount + "_value]");
             newEle.find(".newNameClass").attr("name", "form[radio_" + radioCount + "]");
             break;
-        case 3:
-
+        case 3: //checkbox
             $(".insertDiv").append(checkboxEle);
             var checkboxCount = $(".insertDiv").find(".checkboxBox").length;
-
             newEle = $(".insertDiv .insertBox ").last();
             newEle.find(".textQuestion").attr("name", "form[checkbox_" + checkboxCount + "_value]");
             newEle.find(".newNameClass").attr("name", "form[checkbox_" + checkboxCount + "]");
-            
             break;
         default:
             $(".insertDiv").append(inputEle);
     }
 }
 
+//双击div， 显示input
 function showInput(obj){
+    var value = $(obj).text() == "双击输入选项" || $(obj).text() == "双击输入问题" ? "" : $(obj).text()
     $(obj).hide();
-    $(obj).parent().children(".txtArea").val($(obj).text()).show();
+    $(obj).parent().children(".txtArea").val(value).show();
     $(obj).parent().children(".txtArea").focus();
 }
 
+//双击div，修改后隐藏当前输入框
 function hideInput(obj){
     $(obj).hide();
     var radio = $(obj).parent().find("input[type='radio']");
@@ -90,30 +86,47 @@ function hideInput(obj){
     $(obj).parent().children(".inputArea").text($(obj).val()).show();
 }
 
+//提交表单验证 #TODO非空验证
 function submitForm(obj, flag){
-    var content = $(".insertDiv").html();
-    var data;
-    content = content.replace(/;/g, "");
-    if(flag=="new"){
-        data = $(obj).parents("form").serialize();
-        data = data + "&page[content]=" + content;
-    }else if(flag=="edit"){
-        data = $(obj).parents("form").serialize();
-        "<form"
+    var content = $.trim($(".insertDiv").html());
+    content = content.replace(/;/g, "");  //把分号替换掉，否则表单提交不完全，会被分号隔开
+    if(flag=="submit"){ //新建 或者编辑
+        var dataValue;
+        $("#form_container form").removeAttr("target");
+        $("#form_container #hiddenContent").remove();
+        dataValue = $(obj).parents("form").serialize();
+        dataValue = dataValue + "&page[content]=" + content;
+        $.ajax({
+            url: $(obj).attr("alt"),
+            type: "POST",
+            dataType: "script",
+            data:dataValue,
+            success:function(data){
+            //alert("success")
+            },
+            error:function(data){
+            //alert("error")
+            }
+        })
+    }else{ //预览
+        $("#form_container #hiddenContentContainer").html('<input type="hidden" id="hiddenContent" name="page[content]"/>');
+        $("#form_container form").attr("target", "_blank").attr("action", $(obj).attr("alt"));
+        $("#form_container #hiddenContent").val(content);
+        $("#form_container form").submit();
     }
-    else{
-        data = "page[content]=" + content;
+    
+}
+
+//增加选项
+function addOption(obj, flag){
+    if(flag==2){ //radio
+        $(obj).parents(".radioBox").append(radioOption);
+    }else{ //checkbox
+        $(obj).parents(".checkboxBox").append(checkboxOption);
     }
-    $.ajax({
-         url: $(obj).attr("alt"),
-         type: "POST",
-         dataType: "script",
-         data:data,
-         success:function(data){
-             alert("success")
-         },
-         error:function(data){
-            alert("error")
-         }
-     })
+}
+
+//删除选项
+function deleOption(obj){
+    $(obj).parent().remove();
 }
