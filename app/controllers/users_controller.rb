7e2b461b161a0c7ec4bc8ecd7bceb39675c60481
zhangwei <zhangwei5837@163.com>
2013-11-13 13:33:33 +0800

    zhangwei

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,34 +3,59 @@ class UsersController < ApplicationController
    # before_action :set_user ,only: [:disable,:delete]
 
   def index
-    puts 'index..................................................................'
-    puts params
+    # puts 'index..................................................................'
+    # puts params
     
      case params[:type]
-     when '1' then @type=1; @users=User.paginate(:page=>params[:page],:per_page=>1);
+     when '1' then @type=1; @users=User.paginate(:page=>params[:page],:per_page=>1,:conditions =>"status!=-1")
      when '2' then @type=2; @sites=Site.paginate(:page=>params[:page],:per_page=>1,:conditions => "status=3") #待审核
      when '3' then @type=3; @sites=Site.paginate(:page=>params[:page],:per_page=>1,:conditions => "status=4")  #审核通过 
      when '4' then @type=4; @sites=Site.paginate(:page=>params[:page],:per_page=>1,:conditions => "status=5") #审核不通过
      end 
-    
-    
-    
-    render 'index'
+ 
+     render 'index'
   end
   
   
   
   def disable
-    # puts 'disable.....................................'
+    # puts 'disable.....................................'  
     set_user
+      puts params
+     if @user.update_attribute(:status,0)      #用户状态              1:正常           0：禁用       -1：删除   
+       # flash[:msg]='success!'      
+     else      
+       # flash[:msg]='error!'    
+     end
+     
+     render 'users/change_status'
+    # respond_to do |format|     
+       # format.html{redirect_to "/user/manage/1?page=#{set_page}"}  
+       # format.js   
+    # end   
+  end
+  
+  
+  
+  
+  
+  
+  def enable  
+    # puts 'enable.....................................................................' 
+    set_user
+     
+     if @user.update_attribute(:status,1)       
+       # flash[:msg]='success!'      
+     else      
+       # flash[:msg]='error!'    
+     end
+     
+     render 'users/change_status'
+    # respond_to do |format|     
+      # format.html{redirect_to "/user/manage/1?page=#{set_page}"}    
+       # format.js     
+    # end 
     
-    if @user.update_attribute(:name,'modify') 
-       
-       flash[:msg]='success!'      
-    else      
-       flash[:msg]='error!'    
-    end
-       redirect_to '/user/manage/1'
   end
   
   
@@ -39,14 +64,14 @@ class UsersController < ApplicationController
   def delete
     # puts 'delete........................................'
     set_user
-    if @user.destroy
-      flash[:msg]='success!'
+    if @user.update_attribute(:status,-1)
+      # flash[:msg]='success!'
     else
-      flash[:msg]='error!'
-      
-    end
-    redirect_to '/user/manage/1'
-    
+      # flash[:msg]='error!'     
+    end  
+    respond_to do |format|      
+      format.html{redirect_to "/user/manage/1?page=#{set_page}"}
+    end   
   end
   
   
@@ -56,5 +81,15 @@ class UsersController < ApplicationController
   end
   
   
+   def set_page
+       if(params[:page]=='')
+         return 1
+       else
+         return params[:page]
+       end   
+   end
+
+
+
   
 end
