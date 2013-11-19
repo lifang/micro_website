e154edeeddbd90843e635f4042aa95d668a205a3
amanda <mengfanling825@163.com>
2013-11-19 19:05:49 +0800

    amanda 11-19-19

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,7 +2,7 @@ set :user, "deploy"                            #cap deploy的操作用户
 set :deploy_to, "/opt/projects/micro_website"     #服务器上项目位置
 set :current_path, "#{deploy_to}/current"
 set :shared_path, "#{deploy_to}/shared"
-role :web, "192.168.0.250"                          # Your HTTP server, Apache/etc
-role :app, "192.168.0.250"                          # This may be the same as your `Web` server
-role :db,  "192.168.0.250", :primary => true # This is where Rails migrations will run
+role :web, "116.255.202.113"                          # Your HTTP server, Apache/etc
+role :app, "116.255.202.113"                          # This may be the same as your `Web` server
+role :db,  "116.255.202.113", :primary => true # This is where Rails migrations will run
 set :rails_env, 'production'
