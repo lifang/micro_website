5caffe91544986d268e4fb312f6989513ece9750
amanda <mengfanling825@163.com>
2013-11-20 12:52:38 +0800

    bug 54

--- app/models/site.rb
+++ app/models/site.rb
@@ -2,6 +2,7 @@
 class Site < ActiveRecord::Base
   
   STATUS = {0 => "新建", 1 => "未审核", 2 => "待审核", 3 => "审核通过",4=>"审核不通过"}
+  STATUS_NAME = {:new => 0, :unverified => 1, :wait_verified => 2, :pass_verified => 3, :fail_varified => 4}
   STATUS_VALUE =[0,1,2,3,4]
   
   
