c2cc637b5ec0f14ee6716f50be3f82beaa9c5da6
amanda <mengfanling825@163.com>
2013-11-29 15:50:13 +0800

    add image_text big_image image_stream constant

--- app/models/page.rb
+++ app/models/page.rb
@@ -7,9 +7,9 @@ class Page < ActiveRecord::Base
   attr_accessible :title, :file_name, :types, :site_id, :path_name, :authenticate,:element_relation,:content,:img_path
   attr_accessor :content
   serialize :element_relation
-  TYPES_ARR = ['main', 'sub', 'style', 'form']
-  TYPE_NAMES = {:main => 0, :sub => 1, :style => 2, :form => 3}
-  TYPES = {0 => "主页", 1 => "子页", 2 => "样式表", 3 => "表单"}
+  TYPES_ARR = ['main', 'sub', 'style', 'form', "image_text", "image_stream", "big_image"]
+  TYPE_NAMES = {:main => 0, :sub => 1, :style => 2, :form => 3, :image_text => 4, :image_stream => 5, :big_image => 6}
+  TYPES = {0 => "主页", 1 => "子页", 2 => "样式表", 3 => "表单", 4 => "图文", 5 => "图片流", 6 => "大图"}
 
   TYPES_ARR.each do |type|
     scope type.to_sym, :conditions => { :types => TYPE_NAMES[type.to_sym] }
