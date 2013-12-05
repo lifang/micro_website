0fa5dee1b6af795425186a358827bf5533f6dc5b
wangping <525399584@qq.com>
2013-12-05 11:29:14 +0800

    'format'

--- app/controllers/image_streams_controller.rb
+++ app/controllers/image_streams_controller.rb
@@ -175,7 +175,7 @@ class ImageStreamsController < ApplicationController
 <head>
 <meta charset='utf-8'>
 <meta name='viewport' content='width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no'>
-<title>冯磊微世界</title>
+<title>#{@page.title}</title>
 <script type='text/javascript' src='/allsites/js/jQuery-v1.9.0.js'></script>
 <script type='text/javascript' src='/allsites/js/main.js'></script>
 <script type='text/javascript' src='/allsites/js/jquery.mobile-1.3.2.js'></script>
