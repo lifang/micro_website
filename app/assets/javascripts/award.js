1245724ccdda5f50bdc7ff77abe833cbe0c05719
wangping <525399584@qq.com>
2013-12-25 11:48:37 +0800

    'ada'

--- app/assets/javascripts/award.js
+++ app/assets/javascripts/award.js
@@ -5,6 +5,54 @@ function add_award_item(){
 	award.append(li);
 }
 function remove_award_item(){
+	var ul=$(".award_item");
 	var li=$(".award_item >li");
+	var id=$(li[li.length-1]).find('.award_item_id').val();
+	if(id!=null){
+		ul.prepend($("<input type='hidden' name='remove_id[]' value='"+id+"'>"));
+	}
 	$(li[li.length-1]).remove();
+}
+function cancle_award_page(){
+	$(".second_bg").hide();
+	$(".second_box.addGgl").hide();
+}
+function check_award_form(){
+	var name=$("input[name='name[]']");
+	var content=$("input[name='content[]']");
+	var number=$("input[name='number[]']");
+	if(name.length==0){
+		tishi_alert("奖项设置不能为空！");
+		    return false;
+	}
+	for(var i=0;i<name.length ; i++){
+		if($.trim($(name[i]).val())==""){
+			tishi_alert("奖项名称存在空白！");
+		    return false;
+		}
+		if( $.trim($(content[i]).val())==""){
+			tishi_alert("奖项内容存在空白！");
+		    return false;
+		}
+		if( !$(number[i]).val().match(/[0-9]/)){
+			tishi_alert("奖项数不为空且为数字！");
+		    return false;
+		}
+	}
+	if($.trim($("#award_name").val())==""){
+		tishi_alert("抽奖名称不能为空！");
+		return false;
+	}
+	var date1=$("#datepicker").val();
+	var date2=$("#datepicker1").val();
+	if(date1==""||date2==""){
+		tishi_alert("日期不能为空！");
+		return false;
+	}
+	var num=$("#award_total_number").val();
+	if( !num.match(/[0-9]/)){
+		tishi_alert("奖券数不能为空！并且为数字");
+		return false;
+	}
+    $(".second_content.second_content2 form").submit();
 }
--- app/controllers/awards_controller.rb
+++ app/controllers/awards_controller.rb
@@ -13,21 +13,22 @@ class AwardsController < ApplicationController
 
   def create
     @award=@site.awards.build
-    @award.name=params[:awards][:begin_date]
-    @award.total_number=params[:awards][:total_number]
-    @award.no_operation_number=params[:awards][:total_number]
-    @award.begin_date=params[:awards][:begin_date]
-    @award.end_date=params[:awards][:end_date]
+    @award.name=params[:award][:name]
+    @award.total_number=params[:award][:total_number]
+    @award.no_operation_number=params[:award][:total_number]
+    @award.begin_date=params[:award][:begin_date]
+    @award.end_date=params[:award][:end_date]
 
     if @award.save
-       save_award_item
-       flash[:success]='新建成功'
-       redirect_to site_awards_path
+      save_award_item
+      flash[:success]='新建成功'
+      redirect_to site_awards_path
     else
-       flash[:error]='新建失败'
-       render 'new'
+      flash[:error]='新建失败'
+      render 'new'
     end
   end
+
   def destroy
     @award=Award.find_by_id(params[:id])
     if @award.destroy
@@ -38,23 +39,29 @@ class AwardsController < ApplicationController
       render 'index'
     end
   end
+
   def edit
     @award=Award.find_by_id(params[:id])
   end
+
   def update
     @award=Award.find_by_id(params[:id])
-    name=params[:awards][:begin_date]
-    total_number=params[:awards][:total_number]
-    begin_date=params[:awards][:begin_date]
-    end_date=params[:awards][:end_date]
-    if @award.updates(name:name,total_number:total_number,begin_date:begin_date,end_date:end_date )
-      
+    name=params[:award][:begin_date]
+    total_number=params[:award][:total_number]
+    begin_date=params[:award][:begin_date]
+    end_date=params[:award][:end_date]
+    if @award.update_attributes(name:name,total_number:total_number,begin_date:begin_date,end_date:end_date )
+      update_award_item
+      flash[:success]='更新成功'
+      redirect_to site_awards_path
     else
-      
+      flash[:success]='更新失败'
+      render 'edit'
     end
   end
-  
+
   private
+
   def save_award_item
     name=params[:name]
     content=params[:content]
@@ -65,6 +72,33 @@ class AwardsController < ApplicationController
       award_info.content=content[x]
       award_info.number=number[x]
       award_info.save
+    end
+  end
+
+  def update_award_item
+    remove_id=params[:remove_id]
+    id=params[:info_id]
+    name=params[:name]
+    content=params[:content]
+    number=params[:number]
+    if !name.nil?
+      (0...name.length).each do|x|
+        if !id[x].nil?
+          award_info=AwardInfo.find_by_id(id[x])
+          award_info.update_attributes(name:name[x],content:content[x],number:number[x])
+        else
+        award_info=@award.award_infos.build
+        award_info.name=name[x]
+        award_info.content=content[x]
+        award_info.number=number[x]
+        award_info.save
+        end
+      end
+    end
+    if !remove_id.nil?
+      remove_id.each do |rid|
+        AwardInfo.find_by_id(rid).destroy
+      end
     end
   end
 end
