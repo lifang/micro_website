926307ae18b3fec5778713a83be552ca3e472722
amanda <mengfanling825@163.com>
2014-02-12 16:21:49 +0800

    微信回复

--- app/helpers/award_helper.rb
+++ app/helpers/award_helper.rb
@@ -1,3 +1,4 @@
+#encoding: utf-8
 module AwardHelper
   def get_award_info(award_infos,id)
     award_infos.each do|award_info|
@@ -6,5 +7,21 @@ module AwardHelper
       end
     end
     nil
+  end
+
+  def reply_content(micro_message)
+    if micro_message
+      if micro_message.text?
+        if micro_message.solid_link_flag == MicroMessage::SOLID_LINK[:ggl]
+          "刮刮乐"
+        elsif micro_message.solid_link_flag == MicroMessage::SOLID_LINK[:app]
+          "app登记"
+        else
+          "一条文字"
+        end
+      elsif micro_message.image_text?
+        "一条图文"
+      end
+    end
   end
 end
